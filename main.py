4412d7a7a5be3d36ee608d1f07809263e16dd72f
unknown <j.currier224@gmail.com>
2022-04-08 23:54:24 -0600

    delete red words

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-'''
-wordle solver'''
+import wordList
+
+
+words = []
+deleteWords = []
+
+useExtraWords = input("Would you like to limit the search to the official Wordle wordlist? (y/n)")
+
+if useExtraWords == 'y':
+    words = wordList.tempList
+else:
+    words = wordList.official + wordList.extra
+
+redLetters = input("Which letters have been eliminated from the puzzle? Do not use a sapce or comma to seperate the letters.  ")
+
+
+for letter in redLetters:
+    for myWord in words:
+        if letter in myWord:
+            deleteWords.append(myWord)
+    print(letter, words)
+
+for delWord in deleteWords:
+    try:
+        words.remove(delWord)
+    except: 
+        pass
+        
+        
+
+print(words)
+
+    
